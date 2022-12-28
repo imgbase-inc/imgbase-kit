c0e22dbc1f63131eb4aee02c8c4eaec2377a0a99
odong-Tree <odongnamu.dev@gmail.com>
2022-12-28 11:50:05 +0900

    ✨ HUDSetting 코드 추가

---
+++ Sources/IMProgressHUD/HUDSetting.swift
@@ -0,0 +1,49 @@
+//
+//  HUDSetting.swift
+//  imgbase-kit
+//
+//  Created by Dongbin Kim on 28/12/2022.
+//  Copyright © 2022 ImgBase, Inc. All rights reserved.
+//
+
+import SwiftUI
+
+public enum ContentType {
+    case infiniteRing
+    case image(_ custom: Image)
+    case success
+    case fail
+}
+
+public enum BackgroundType {
+    case black
+    case none
+    case blur
+}
+
+public class HUDSetting: ObservableObject {
+    @Published var contentType: ContentType = .infiniteRing
+    @Published var textString: String?
+    @Published var textColor: Color = .black.opacity(0.8)
+    @Published var textFont: Font = .body
+    @Published var foregroundColor: Color = .blue
+    @Published var backgroundColor: Color = .white
+    @Published var minimumSize: CGSize = .zero
+    @Published var imageViewSize: CGSize = CGSize(width: 40, height: 40)
+    @Published var cornerRadius: CGFloat = 10
+    @Published var ringThickness: CGFloat = 0
+    @Published var successImage: Image = Image(systemName: "checkmark")
+    @Published var errorImage: Image = Image(systemName: "xmark.circle")
+    @Published var maximumDismissTimeInterval: TimeInterval = 3.0
+    @Published var minimumDismissTimeInterval: TimeInterval = 0.5
+    @Published var isUserInteractionEnabled: Bool = true
+    @Published var backgroundType: BackgroundType = .none
+
+    var displayDurationForString: TimeInterval {
+        get {
+            let max = max(Double(textString?.count ?? 0) * 0.06 + 0.5, minimumDismissTimeInterval)
+            return min(max, maximumDismissTimeInterval)
+        }
+    }
+}
+
