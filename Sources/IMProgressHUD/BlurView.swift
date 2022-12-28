6c3ee9710b8777ad44021b25f9226f795b1e9f5c
odong-Tree <odongnamu.dev@gmail.com>
2022-12-28 11:50:33 +0900

    ✨ BlurView 코드 추가

--- Sources/IMProgressHUD/BlurView.swift
+++ Sources/IMProgressHUD/BlurView.swift
@@ -1,8 +1,31 @@
 //
-//  File.swift
-//  
+//  BlurView.swift
+//  imgbase-kit
 //
-//  Created by odong on 2022/12/28.
+//  Created by Dongbin Kim on 28/12/2022.
+//  Copyright © 2022 ImgBase, Inc. All rights reserved.
 //
 
-import Foundation
+import SwiftUI
+
+internal struct BlurView: View {
+    private struct BackdropView: UIViewRepresentable {
+        func makeUIView(context: Context) -> UIVisualEffectView {
+            let view = UIVisualEffectView()
+            let blur = UIBlurEffect(style: .extraLight)
+            let animator = UIViewPropertyAnimator()
+            animator.addAnimations { view.effect = blur }
+            animator.fractionComplete = 0
+            animator.stopAnimation(true)
+            animator.finishAnimation(at: .start)
+            return view
+        }
+
+        func updateUIView(_ uiView: UIVisualEffectView, context: Context) { }
+    }
+
+    var body: some View {
+        BackdropView().blur(radius: 2)
+    }
+}
+
