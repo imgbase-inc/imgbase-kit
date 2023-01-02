3f31a97b6d506a5391bad59e5f5f167f8660e559
odong-Tree <odongnamu.dev@gmail.com>
2023-01-02 16:31:15 +0900

    ✨ IMProgressHUD 예시 실행 코드 추가

--- Examples/IMProgressHUD_Example/IMProgressHUD_Example/ViewController.swift
+++ Examples/IMProgressHUD_Example/IMProgressHUD_Example/ViewController.swift
@@ -6,35 +6,57 @@
 //
 
 import UIKit
+import IMProgressHUD
 
+// MARK: LayoutConstants
 extension ViewController {
     private struct LayoutConstants {
         static let tableViewHeaderViewHeight: CGFloat = 30
     }
 }
 
+extension ViewController {
+    private enum Selection: String {
+        case infiniteRing = "InfiniteRing"
+        case infiniteRingWithText = "InfiniteRing Text"
+        case customImage = "Custom Image"
+        case customImageWithText = "Custom Image + Text"
+        case dismiss = "Dismiss"
+        case success = "Success"
+        case fail = "Fail"
+        case noneBackground = "None"
+        case blackBackground = "Black"
+        case blurBackground = "Blur"
+        case userInteratable = "True"
+        case notUserInteractable = "False"
+    }
+}
+
 final class ViewController: UIViewController {
-    typealias SelectionGroup = (groupName: String, selections: [String])
+    private typealias SelectionGroup = (groupName: String, selections: [Selection])
 
     private let selection: [SelectionGroup] = [
         ("Basic", [
-            "InfiniteRing",
-            "InfiniteRing + Text",
-            "Custom Image",
-            "Custom Image + Text"
+            .infiniteRing,
+            .infiniteRingWithText,
+            .customImage,
+            .customImageWithText
         ]),
-        ("Success/Fail", [
-            "InfiniteRing + Success",
-            "InfiniteRing + Fail"
+        ("Dismiss", [
+            .dismiss
+        ]),
+        ("Success / Fail", [
+            .success,
+            .fail
         ]),
         ("Background", [
-            "None",
-            "Black",
-            "Blur"
+            .noneBackground,
+            .blackBackground,
+            .blurBackground
         ]),
         ("User Interaction", [
-            "True",
-            "False"
+            .userInteratable,
+            .notUserInteractable
         ])
     ]
 
@@ -45,6 +67,17 @@ final class ViewController: UIViewController {
 
         tableView.dataSource = self
         tableView.delegate = self
+
+        setHUDSettings()
+    }
+
+    private func setHUDSettings() {
+        IMProgressHUD.setForegroundColor(UIColor.orange)
+        IMProgressHUD.setMinimumSize(CGSize(width: 100, height: 100))
+        IMProgressHUD.setBackgroundColor(UIColor.systemGray5)
+        IMProgressHUD.setCornerRadius(10)
+        IMProgressHUD.setImageViewSize(CGSize(width: 70, height: 70))
+        IMProgressHUD.setRingThickness(4)
     }
 }
 
@@ -67,13 +100,52 @@ extension ViewController: UITableViewDataSource {
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
-
-        cell.textLabel?.text = selection[indexPath.section].selections[indexPath.row]
+        cell.textLabel?.text = selection[indexPath.section].selections[indexPath.row].rawValue
 
         return cell
     }
 }
 
 extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selection = selection[indexPath.section].selections[indexPath.row]
 
+        switch selection {
+        case .infiniteRing:
+            IMProgressHUD.show()
+        case .infiniteRingWithText:
+            IMProgressHUD.show(text: "Loading...")
+        case .customImage:
+            IMProgressHUD.show(with: UIImage(systemName: "figure.walk.diamond") ?? UIImage())
+        case .customImageWithText:
+            IMProgressHUD.show(with: UIImage(systemName: "figure.walk.diamond") ?? UIImage(), text: "Loading...")
+        case .dismiss:
+            IMProgressHUD.dismiss()
+        case .success:
+            IMProgressHUD.showSuccess()
+        case .fail:
+            IMProgressHUD.showFail()
+        case .noneBackground:
+            IMProgressHUD.show()
+        case .blackBackground:
+            IMProgressHUD.show(backgroundType: .black)
+            hudDismissAsyncAfter { IMProgressHUD.showSuccess() }
+        case .blurBackground:
+            IMProgressHUD.show(backgroundType: .blur)
+            hudDismissAsyncAfter { IMProgressHUD.showSuccess() }
+        case .userInteratable:
+            IMProgressHUD.show(isUserInteractionEnabled: true)
+        case .notUserInteractable:
+            IMProgressHUD.show(isUserInteractionEnabled: false)
+            hudDismissAsyncAfter { IMProgressHUD.showSuccess() }
+        }
+    }
+
+    private func hudDismissAsyncAfter(deadline: DispatchTime = .now() + 1.5, completion: @escaping () -> Void) {
+        DispatchQueue.global().asyncAfter(deadline: deadline) {
+            DispatchQueue.main.async {
+                completion()
+            }
+        }
+    }
 }
