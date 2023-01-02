e63917d7031b4faef4c5ec4e538ddd2706b4ef6f
odong-Tree <odongnamu.dev@gmail.com>
2023-01-02 12:54:50 +0900

    ✨ TableView 리스트 구현

--- Examples/IMProgressHUD_Example/IMProgressHUD_Example/ViewController.swift
+++ Examples/IMProgressHUD_Example/IMProgressHUD_Example/ViewController.swift
@@ -7,10 +7,73 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+extension ViewController {
+    private struct LayoutConstants {
+        static let tableViewHeaderViewHeight: CGFloat = 30
+    }
+}
+
+final class ViewController: UIViewController {
+    typealias SelectionGroup = (groupName: String, selections: [String])
+
+    private let selection: [SelectionGroup] = [
+        ("Basic", [
+            "InfiniteRing",
+            "InfiniteRing + Text",
+            "Custom Image",
+            "Custom Image + Text"
+        ]),
+        ("Success/Fail", [
+            "InfiniteRing + Success",
+            "InfiniteRing + Fail"
+        ]),
+        ("Background", [
+            "None",
+            "Black",
+            "Blur"
+        ]),
+        ("User Interaction", [
+            "True",
+            "False"
+        ])
+    ]
+
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        tableView.dataSource = self
+        tableView.delegate = self
     }
+}
+
+extension ViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return selection.count
+    }
+
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return selection[section].groupName
+    }
+
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return LayoutConstants.tableViewHeaderViewHeight
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return selection[section].selections.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+
+        cell.textLabel?.text = selection[indexPath.section].selections[indexPath.row]
+
+        return cell
+    }
+}
+
+extension ViewController: UITableViewDelegate {
+
 }
