30f9928ab55e8034e06a32bcc125b4ec3bb1ee86
odong-Tree <odongnamu.dev@gmail.com>
2023-03-16 11:06:44 +0900

    ♻ InfinityRingView 머리 방향 수정

--- Sources/IMProgressHUD/AnimationView/InfinityRingView.swift
+++ Sources/IMProgressHUD/AnimationView/InfinityRingView.swift
@@ -15,7 +15,7 @@ internal struct InfinityRingView: View {
     @State private var rotationSpeed = 0.9
 
     private var gradient: AngularGradient {
-        AngularGradient(gradient: Gradient(colors: [hudSetting.foregroundColor, .clear]),
+        AngularGradient(gradient: Gradient(colors: [.clear, hudSetting.foregroundColor]),
                                             center: .center,
                                             startAngle: .degrees(0),
                                             endAngle: .degrees(360))
